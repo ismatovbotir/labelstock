1376b8560f346c0432ce590992e002c61d1df4dc
Botir Ismatov <botirismatov@MacBook-Pro-Botir.local>
2024-11-30 22:08:07 +0500

    migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('mob')->nullable();
+            $table->foreignId('role_id')->default('2');
+            $table->foreignId('company_id')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
